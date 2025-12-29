fc2c099c088f60e99b4d4c03f866600c114bd095
Shagun Sharma <119945335+Shagunsharmacs22@users.noreply.github.com>
2025-12-29 12:24:23 +0530

    Update FestivalLuckyDraw.java

--- core-java-practice/scenario-based/FestivalLuckyDraw.java
+++ core-java-practice/scenario-based/FestivalLuckyDraw.java
@@ -1,10 +1,27 @@
 package ScenarioBased;
-
+import java.util.Scanner;
 public class FestivalLuckyDraw {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter your number (or type 'exit' to quit):");
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Thank you for participating!");
+                break;
+            }
+            int num;
+            try {
+                num= Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number.");
+                continue;
+            }
+            if (num% 3 == 0 && num% 5 == 0) {
+                System.out.println("Congratulations! You win a gift!");
+            } else {
+                System.out.println("Sorry, better luck next time!");
+            }
+        }
+    }
 }
