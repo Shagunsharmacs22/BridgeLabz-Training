0d2018f759daec24be99121165151c32ec5cbb98
shagunsharmacs22 <shagunmanishsharma@gmail.com>
2025-12-29 10:46:04 +0530

    [Shagun].Add scenario based question .

---
+++ core-java-practice/scenario-based/BusRouteDistanceTracker.java
@@ -0,0 +1,10 @@
+package ScenarioBased;
+
+public class BusRouteDistanceTracker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ core-java-practice/scenario-based/FestivalLuckyDraw.java
@@ -0,0 +1,10 @@
+package ScenarioBased;
+
+public class FestivalLuckyDraw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
