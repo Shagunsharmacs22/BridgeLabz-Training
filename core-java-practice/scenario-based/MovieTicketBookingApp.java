4f0aa041682d208feed5ae5f4bbafcee9e9c1dfe
shagunsharmacs22 <shagunmanishsharma@gmail.com>
2025-12-29 11:42:26 +0530

    [Shagun].Add scenario based questions.

---
+++ core-java-practice/scenario-based/MovieTicketBookingApp.java
@@ -0,0 +1,10 @@
+package ScenarioBased;
+
+public class MovieTicketBookingApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ core-java-practice/scenario-based/RohanLibraryReminderApp.java
@@ -0,0 +1,10 @@
+package ScenarioBased;
+
+public class RohanLibraryReminderApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
