4edc0c4328d85c9973b41da206141ce454252659
shagunsharmacs22 <shagunmanishsharma@gmail.com>
2025-12-30 15:22:47 +0530

    [Shagun].Update UC3 of snake and ladder problem

--- core-java-practice/scenario-based/SnakeAndLadder.java
+++ core-java-practice/scenario-based/SnakeAndLadder.java
@@ -12,6 +12,21 @@ public class SnakeAndLadder {
 				Random random=new Random();
 				int dice=random.nextInt(6)+1;
 				System.out.println("Dice rolled : "+dice);
+				
+				//Use case 3 : Player checks for the options between no play , ladder or snake bite
+				int option=random.nextInt(3);
+				if(option==0) {
+					System.out.println("Option : No play");
+				}
+				else if(option==1) {
+					System.out.println("Option : Ladder ");
+					playerPosition+=dice;
+				}
+				else {
+					System.out.println("Option:Snake");
+					playerPosition-=dice;
+				}
+				System.out.println("New Position : "+playerPosition);
 	}
 
 }
