d197c824fc5d04a7fc8597640afde2ea156e8d7a
shagunsharmacs22 <shagunmanishsharma@gmail.com>
2026-01-03 15:23:56 +0530

    Deletion of folders

--- core-java-practice/gcr-codebase/programming-elements/KiloToMiles.java
+++
@@ -1,9 +0,0 @@
-package level1;
-
-public class KiloToMiles {
-    public static void main(String[] args) {
-        double km = 10.8; 
-        double miles = km * 1.6;
-        System.out.println("The Distance "+ km+ "  in miles is "+ miles);
-    }
-}
