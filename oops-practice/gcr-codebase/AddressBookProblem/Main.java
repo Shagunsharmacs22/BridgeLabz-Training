5cfaca8dc155d8b2edc0fc4264e3f0966bace4ad
shagunsharmacs22 <shagunmanishsharma@gmail.com>
2026-01-17 12:18:22 +0530

    [Shagun].Add Uc6 of addressBookproblem

--- oops-practice/gcr-codebase/AddressBookProblem/Main.java
+++ oops-practice/gcr-codebase/AddressBookProblem/Main.java
@@ -6,18 +6,32 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Welcome to Address Book Program in AddressBook Main class on Master Branch");
-		 AddressBook addressBook = new AddressBook();
+		 AddressBookContainer container = new AddressBookContainer();
+	      AddressBook currentBook = null;
 		 int choice = 0;
-	        while (choice != 4) {
-	            System.out.println("\nADDRESS BOOK MENU");
-	            System.out.println("1. Create New Contact");
-	            System.out.println("2. Edit Contact by Name");
-	            System.out.println("3. Delete Contact by Name");
-	            System.out.println("4. Exit");
+	        while (choice != 5) {
+	        	 System.out.println("\nADDRESS BOOK MENU");
+	             System.out.println("1. Create New Address Book");
+	             System.out.println("2. Create New Contact");
+	             System.out.println("3. Edit Contact by Name");
+	             System.out.println("4. Delete Contact by Name");
+	             System.out.println("5. Exit");
 	            System.out.print("Enter your choice: ");
 	            choice = scanner.nextInt();
 	            switch (choice) {
-	                case 1:
+	            case 1:
+                    currentBook = new AddressBook();
+                    System.out.print("Enter Address Book Name: ");
+                    String bookName = scanner.next();
+                    container.addBook(bookName, currentBook);
+                    System.out.println("📘 AddressBook '" + bookName + "' created & selected");
+                    break;
+ 
+	                case 2:
+	                	 if (currentBook == null) {
+	                         System.out.println("❌ Please create AddressBook first");
+	                         break;
+	                     }
 	                    System.out.println("Create New Contact!");
 	                    
 	                    System.out.println("Enter First Name: ");
@@ -45,31 +59,30 @@ public class Main {
 	                    String email = scanner.next();
 
 	                    Contact contact = new Contact(  firstName,lastName,address,city,state,zip,phoneNumber, email);
-	                    addressBook.addContact(contact);
-	                    addressBook.displayAllContacts();
+	                    currentBook.addContact(contact);
 	                    break;
 	                    
-	                case 2:
+	                case 3:
 	                    System.out.print("Enter First Name to Edit: ");
 	                    String name = scanner.next();
-	                    addressBook.editContactByName(name, scanner);
+	                    currentBook.editContactByName(name, scanner);
 	                    break;
 	                
-	                case 3:
+	                case 4:
 	                    System.out.print("Enter First Name to Delete: ");
 	                    String deleteName = scanner.next();
-	                    addressBook.deleteContactByName(deleteName);
+	                    currentBook.deleteContactByName(deleteName);
 	                    break;
 	                    
-	                case 4:
+	                case 5:
 	                    System.out.println("Exiting Address Book. Goodbye!");
 	                    break;
 
 	                default:
-	                    System.out.println("❌ Invalid choice. Please enter 1 or 2.");
+	                    System.out.println("❌ Invalid choice. Please enter 1,2,3,4,5");
 	            }
 	        }
-	        addressBook.displayAllContacts();
+	        container.displayAllBook();
 	}
 
 }
